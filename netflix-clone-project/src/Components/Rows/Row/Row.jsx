a5023f24ccefe96de8852d6c44b1465a3b1342c2
yeniesew <yebrielij@gmail.com>
2024-11-24 16:23:03 -0800

    banner

---
+++ netflix-clone-project/src/Components/Rows/Row/Row.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../"
+
+function Row() {
+  return (
+    <div>Row</div>
+  )
+}
+
+export default Row
--- netflix-clone-project/src/Pages/Home/Home.jsx
+++ netflix-clone-project/src/Pages/Home/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
+import Banner from './../../Components/Banner/Banner';
 
 
 function Home() {
   return (
-    <>
+    <div>
     <Header/>
+    <Banner/>
     <Footer/>
-    </>
+    </div>
   )
 }
 
